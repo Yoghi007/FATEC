61fb654b0e54b67b09a003e25cfc9cd2629a6610
Gabriel Lima dos Santos <gabriellimadc1@hotmail.com>
2023-11-22 00:08:09 -0300

    Adição do CSS, JS, JQUERY e Pooper ao projeto

---
+++ js/funcoes.js
@@ -0,0 +1,13 @@
+function AlertaCadastro(){
+    alert("Cadastro Efetuado com sucesso! Voltando");
+}
+
+function DesabilitaDiv(boo){
+const elemento = document.getElementById("RegistroSucesso");
+if (boo ===""){
+    boo = false;
+}
+ return elemento = boo;
+
+}
+
